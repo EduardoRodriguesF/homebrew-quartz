3349cbe37d3e641614e4ddf12e92769b470d34b1
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-08-12 15:32:33 -0300

    quartz 0.8.0

--- Formula/quartz.rb
+++ Formula/quartz.rb
@@ -1,8 +1,8 @@
 class Quartz < Formula
   desc "The CLI way to build and test Rest APIs"
   homepage "https://github.com/EduardoRodriguesF/quartz"
-  url "https://github.com/EduardoRodriguesF/quartz/releases/download/v0.7.1/quartz-v0.7.1-x86_64-apple-darwin.tar.gz"
-  sha256 "8b99eb0dadf0b94a2aa6ff7bd1c9602efb8060f01a87cf183e2eb5e5f3de449a"
+  url "https://github.com/EduardoRodriguesF/quartz/releases/download/v0.8.0/quartz-v0.8.0-x86_64-apple-darwin.tar.gz"
+  sha256 "080633d7ca6a3b83fe298fdfb353db10a19ffb68df5b3e24d6050b4c1dc4dfb1"
 
   def install
     bin.install "quartz"
