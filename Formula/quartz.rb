cc493e752b367373c02b9e819b6ef813218ca14c
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-03-09 16:10:23 -0300

    update description

--- Formula/quartz.rb
+++ Formula/quartz.rb
@@ -1,5 +1,5 @@
 class Quartz < Formula
-  desc "The CLI way to build and test Rest APIs"
+  desc "The command-line tool to build and test HTTP requests"
   homepage "https://github.com/EduardoRodriguesF/quartz"
   url "https://github.com/EduardoRodriguesF/quartz/releases/download/v0.8.0/quartz-v0.8.0-x86_64-apple-darwin.tar.gz"
   sha256 "080633d7ca6a3b83fe298fdfb353db10a19ffb68df5b3e24d6050b4c1dc4dfb1"
